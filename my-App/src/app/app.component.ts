8c7466a35fa6d7b09814f24499f64355c6fc5967
PoonamGaikwad8088 <poonam8088@gmail.com>
2020-03-06 20:06:56 +0530

    code commits

--- my-App/src/app/app.component.ts
+++ my-App/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as introJs from 'intro.js/intro.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-App';
+  introJS = introJs();
+ 
+ 
+ 
+  constructor() {
+    
+  }
+ 
+  ngOnInit() {
+    this.introJS.start().goToStep(3);
+    console.log(1 + false);
+  }
+ 
 }
